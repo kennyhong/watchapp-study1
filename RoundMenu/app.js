c14d589fedd2a9f9c11ba437fbbe89edab529e07
Teng Han <hanteng1021@outlook.com>
2018-05-16 19:37:55 -0500

    js animation works

--- RoundMenu/app.js
+++ RoundMenu/app.js
@@ -1,4 +1,7 @@
-(function () {
+"use strict";
+
+window.onload = function () {
+
 	window.addEventListener("tizenhwkey", function (ev) {
 		var activePopup = null,
 			page = null,
@@ -20,5 +23,52 @@
 		}
 	});
 
-}());
+
+	//touch screen event
+	document.addEventListener("click", function(){
+
+		let text = document.querySelector('p');
+    	text.innerHTML = "new test";
+
+		animate(
+    		1000,
+    		draw, 
+    		timing
+    	);
+
+
+	});
+
+	function draw(progress){
+		progressId.style.width = progress * 100 + '%';
+	}
+
+	function timing(timeFraction){
+		return timeFraction;
+	}
+
+	document.addEventListener("rotarydetent", function(){
+
+	});
+
+
+	function animate(duration, draw, timing){
+    	let start = performance.now();
+
+    	requestAnimationFrame(function animate(time){
+    		let timeFraction = (time - start) / duration;
+    		if(timeFraction > 1) timeFraction = 1;
+    		
+    		let progress = timing(timeFraction);
+    		
+    		draw(progress);
+    		
+    		if(timeFraction < 1)
+    		{
+    			requestAnimationFrame(animate);
+    		}
+    	});
+    }
+
+};
 
