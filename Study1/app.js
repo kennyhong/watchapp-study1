dee05159c0ff74c2d419ea5ca897a4ab5a50b967
KennyHong <k.hongjiahan@gmail.com>
2018-09-05 10:21:33 -0500

    Training added

--- Study1/app.js
+++ Study1/app.js
@@ -14,6 +14,10 @@ var state;
 var failFeedback;
 var successFeedback;
 
+var isTraining = true;
+var currTraining = 0;
+var trainingTargets = [1,4,7,6,3,2,8,5];
+
 var eyesFree = true;
 var menuVisible = true;
 
@@ -116,7 +120,7 @@ function setupStudy() {
 					dataFile.openStream("w", function(fs) {
 						fs.write("timestamp,participant_id,trial_number,block,motor_condition,visual_condition," +
 								"id_current_selection,current_selection,id_target_selection,target_selection," +
-								"event_type,x,y,angle,total_angle,num_+15,num_-15,is_eyes_free,is_menu_visible\n");
+								"event_type,x,y,angle,total_angle,num_+15,num_-15,is_menu_visible,training_mode\n");
 						fs.close();
 					}, function(e) {
 						console.log("Error " + e.message);
@@ -130,7 +134,7 @@ function setupStudy() {
 					eventsFile.openStream("w", function(fs) {
 						fs.write("participant_id,trial_number,block,motor_condition,visual_condition," +
 								"id_current_selection,current_selection,id_target_selection,target_selection,target_angle," +
-								"selection_success,num_overshoots,selection_time,first_rotation_time,total_rotation_angle,num_+15,num_-15,is_eyes_free,is_menu_visible\n");
+								"selection_success,num_overshoots,selection_time,first_rotation_time,total_rotation_angle,num_+15,num_-15,is_menu_visible,training_mode\n");
 						fs.close();
 					}, function(e) {
 						console.log("Error " + e.message);
@@ -167,7 +171,7 @@ function setupStudy() {
 				//Save Next Participant
 				saveParticipant();
 			
-				state.innerHTML = "Start";
+				state.innerHTML = "Tutorial Start";
 			},
 			function(error) {
 				console.log(JSON.stringify(error));
@@ -226,17 +230,30 @@ function log(data) {
 	var currItem;
 	var targetItem;
 
-	if (rotationCount > -1) {
-		currItem = cleanImageFilePath(imgPaths[conditionNum][rotationCount]);
+	if (isTraining && inTrial) {
+		if (rotationCount > -1) {
+			currItem = cleanImageFilePath(imgPaths[1][rotationCount]);
+		} else {
+			currItem = "unselected";
+		}
+		if ((trainingTargets[currTraining] - 1) > -1 && inTrial) {
+			targetItem = cleanImageFilePath(imgPaths[1][trainingTargets[currTraining] - 1]);
+		} else {
+			targetItem = "no_target";
+		}
 	} else {
-		currItem = "unselected";
+		if (rotationCount > -1) {
+			currItem = cleanImageFilePath(imgPaths[conditionNum][rotationCount]);
+		} else {
+			currItem = "unselected";
+		}
+		if ((data.target_id - 1) > -1 && inTrial) {
+			targetItem = cleanImageFilePath(imgPaths[conditionNum][data.target_id - 1]);
+		} else {
+			targetItem = "no_target";
+		}
 	}
 
-	if ((data.target_id - 1) > -1 && inTrial) {
-		targetItem = cleanImageFilePath(imgPaths[conditionNum][data.target_id - 1]);
-	} else {
-		targetItem = "no_target";
-	}
 	if (dataFile !== null) {
 		dataFile.openStream("a", function(fs) {
 			fs.write(data.timestamp + "," + participantId
@@ -246,7 +263,7 @@ function log(data) {
 					+ data.selection_id + "," + currItem + "," + data.target_id
 					+ "," + targetItem + "," + data.event_type + "," + data.x
 					+ "," + data.y + "," + data.angle + "," + data.total_angle + "," + num_cw + "," + num_ccw
-					+ "," + data.curr_ef + "," + menuVisible + "\n");
+					+ "," + menuVisible + "," + isTraining + "\n");
 			fs.close();
 		}, function(e) {
 			console.log("Error " + e.message);
@@ -268,18 +285,30 @@ function logSummary(data) {
 	} else {
 		targetAngle = -1;
 	}
-
-	if (rotationCount > -1) {
-		currItem = cleanImageFilePath(imgPaths[conditionNum][rotationCount]);
+	if (isTraining && inTrial) {
+		if (rotationCount > -1) {
+			currItem = cleanImageFilePath(imgPaths[1][rotationCount]);
+		} else {
+			currItem = "unselected";
+		}
+		if ((trainingTargets[currTraining] - 1) > -1 && inTrial) {
+			targetItem = cleanImageFilePath(imgPaths[1][trainingTargets[currTraining] - 1]);
+		} else {
+			targetItem = "no_target";
+		}
 	} else {
-		currItem = "unselected";
+		if (rotationCount > -1) {
+			currItem = cleanImageFilePath(imgPaths[conditionNum][rotationCount]);
+		} else {
+			currItem = "unselected";
+		}
+		if ((data.target_id - 1) > -1 && inTrial) {
+			targetItem = cleanImageFilePath(imgPaths[conditionNum][data.target_id - 1]);
+		} else {
+			targetItem = "no_target";
+		}
 	}
-
-	if ((data.target_id - 1) > -1 && inTrial) {
-		targetItem = cleanImageFilePath(imgPaths[conditionNum][data.target_id - 1]);
-	} else {
-		targetItem = "no_target";
-	}
+		
 	if (eventsFile !== null) {
 		eventsFile.openStream("a", function(fs) {
 			fs.write(participantId + "," + currTrial + "," + data.curr_block + ","
@@ -288,7 +317,7 @@ function logSummary(data) {
 					+ data.selection_id + "," + currItem + "," + data.target_id
 					+ "," + targetItem + "," + targetAngle + "," + data.success + "," + data.num_overshoots + "," + data.timestamp
 					+ "," + firstRotationTime + "," + data.angle + "," + data.num_pos15 + "," + data.num_neg15 
-					+ "," + data.curr_ef + "," + menuVisible + "\n");
+					+ "," + menuVisible + "," + isTraining + "\n");
 			fs.close();
 		}, function(e) {
 			console.log("Error " + e.message);
@@ -321,7 +350,9 @@ function clickEvent(event) {
 	log(data);
 	console.log("timestamp: " + (Date.now() - currDate) + " type: "
 			+ event.type + " x: " + event.clientX + " y: " + event.clientY);
-	if (inTrial) {
+	if (isTraining && inTrial) {
+		tutSelectionCheck(event.clientX, event.ClientY);
+	} else if (inTrial) {
 		selectionCheck(event.clientX, event.clientY);
 	} 
 	toggleTrial();
@@ -330,7 +361,26 @@ function clickEvent(event) {
 }
 
 function toggleTrial() {
-	if (state.innerHTML === "Start") {
+	if (state.innerHTML === "Tutorial Start") {
+		rotationCount = 0;
+		num_cw = 0;
+		num_ccw = 0;
+		currDate = Date.now();
+		window.addEventListener("rotarydetent", tutorialRotaryEventHandler);
+		state.innerHTML = "";
+		inTrial = true;
+		resetMenu();
+		setTutorialMenuLayout();
+		loadTutorialTarget();
+		motorRotationCount = 0;
+		$("#ss_menu").show();
+		highLight(rotationCount, ss_menus);
+		if (trials[currTrial].eyesFreeCondition == true) {
+			$('#ss_menu').hide();
+			menuVisible = false;
+			firstRotationBool = false;
+		}
+	} else if (state.innerHTML === "Start") {
 		rotationCount = 0;
 		num_cw = 0;
 		num_ccw = 0;
@@ -342,7 +392,7 @@ function toggleTrial() {
 		setMenuLayout();
 		loadTarget();
 		motorRotationCount = 0;
-		document.querySelector("#ss_menu").style.visibility = "visible";
+		$("#ss_menu").show();
 		highLight(rotationCount, ss_menus);
 		if (trials[currTrial].eyesFreeCondition == true) {
 			$('#ss_menu').hide();
@@ -356,7 +406,6 @@ function toggleTrial() {
 			currCondition.innerHTML = "Motor: " + trials[currTrial].motorCondition + " ticks";
 			document.querySelector("#target-img").style.visibility = "hidden";
 			$("#target-img").attr('src', '');
-			resetMenu();
 		} else {
 			document.querySelector("#target-img").style.visibility = "hidden";
 			hideMenu();
@@ -369,7 +418,7 @@ function toggleTrial() {
 
 function delayMenuAppearance() {
 	clearTimeout(timeoutHandler);
-	timeoutHandler = setTimeout(function () {$('#ss_menu').show(); menuVisible = true}, 500);
+	timeoutHandler = setTimeout(function () {$('#ss_menu').show(); menuVisible = true}, 750);
 }
 
 function rotaryEventHandler(event) {
@@ -388,7 +437,7 @@ function rotaryEventHandler(event) {
 			navigator.vibrate(50);
 			if(inTrial && !firstRotationBool) {
 				firstRotationTime = Date.now() - currDate;
-				timeoutHandler = setTimeout(function () {$('#ss_menu').show(); menuVisible = true}, 500);
+				timeoutHandler = setTimeout(function () {$('#ss_menu').show(); menuVisible = true}, 750);
 				firstRotationBool = true;
 			} else {
 				delayMenuAppearance();
@@ -434,7 +483,7 @@ function rotaryEventHandler(event) {
 			navigator.vibrate(50);
 			if(inTrial && !firstRotationBool) {
 				firstRotationTime = Date.now() - currDate;
-				timeoutHandler = setTimeout(function () {$('#ss_menu').show(); menuVisible = true}, 500);
+				timeoutHandler = setTimeout(function () {$('#ss_menu').show(); menuVisible = true}, 750);
 				firstRotationBool = true;
 			} else {
 				delayMenuAppearance();
@@ -476,6 +525,110 @@ function rotaryEventHandler(event) {
 	}
 }
 
+function tutorialRotaryEventHandler(event) {
+	let direction = event.detail.direction;
+	var data;
+	var prevItem = '';
+	if (direction === 'CW') {
+		num_cw++;
+		if (motorRotationCount > -1 && !turnRight) {
+			motorRotationCount = 0
+			turnRight = true;
+		}
+		currAngle += 15;
+		motorRotationCount++;
+		if (motorRotationConditions[trials[currTrial].motorCondition - 1] == motorRotationCount) {
+			navigator.vibrate(50);
+			if(inTrial && !firstRotationBool) {
+				firstRotationTime = Date.now() - currDate;
+				timeoutHandler = setTimeout(function () {$('#ss_menu').show(); menuVisible = true}, 750);
+				firstRotationBool = true;
+			} else {
+				delayMenuAppearance();
+			}
+			if (rotationCount !== -1) {
+				prevItem = imgPaths[1][rotationCount];
+			}
+			rotationCount++;
+			if (prevItem !== '') {
+				checkOvershoot(prevItem);
+			}
+			motorRotationCount = 0;
+			if (rotationCount == ss_num) {
+				rotationCount = 0;
+			}
+		}
+		data = {
+			timestamp : Date.now() - currDate,
+			event_type : "rotary",
+			x : 0,
+			y : 0,
+			angle : '+15',
+			total_angle: currAngle,
+			selection_id: rotationCount + 1,
+			target_id : trainingTargets[currTraining],
+			curr_motor: trials[currTrial].motorCondition,
+			curr_visual: trials[currTrial].visualCondition,
+			curr_block: 0,
+			curr_ef: trials[currTrial].eyesFreeCondition
+		};
+		log(data);
+		console.log("timestamp: " + (Date.now() - currDate) + " type: "
+				+ "rotary" + " current angle: " + currAngle);
+
+	} else if (direction === 'CCW') {
+		num_ccw++;
+		if (motorRotationCount > -1 && turnRight) {
+			motorRotationCount = 0
+			turnRight = false;
+		}
+		motorRotationCount++;
+		if (motorRotationConditions[trials[currTrial].motorCondition - 1] == motorRotationCount) {
+			navigator.vibrate(50);
+			if(inTrial && !firstRotationBool) {
+				firstRotationTime = Date.now() - currDate;
+				timeoutHandler = setTimeout(function () {$('#ss_menu').show(); menuVisible = true}, 750);
+				firstRotationBool = true;
+			} else {
+				delayMenuAppearance();
+			}
+			if (rotationCount !== -1) {
+				var prevItem = imgPaths[conditionNum][rotationCount];
+			}
+			rotationCount--;
+			if (prevItem !== '') {
+				checkOvershoot(prevItem);
+			}
+			if (rotationCount < 0) {
+				rotationCount = ss_num - 1;
+			}
+			motorRotationCount = 0;
+		}
+		currAngle -= 15;
+		data = {
+			timestamp : Date.now() - currDate,
+			event_type : "rotary",
+			x : 0,
+			y : 0,
+			angle : '-15',
+			total_angle : currAngle,
+			selection_id: rotationCount + 1,
+			target_id : trainingTargets[currTraining],
+			curr_motor: trials[currTrial].motorCondition,
+			curr_visual: trials[currTrial].visualCondition,
+			curr_block: 0,
+			curr_ef: trials[currTrial].eyesFreeCondition
+		};
+		log(data);
+		console.log("timestamp: " + (Date.now() - currDate) + " type: "
+				+ "rotary" + " current angle: " + currAngle);
+	}
+	// console.log(rotationCount);
+	if (rotationCount > -1) {
+		highLight(rotationCount, ss_menus);
+	}
+}
+
 function checkOvershoot(prevItem) {
 	if (prevItem.includes(imgPaths[conditionNum][trials[currTrial].target - 1])) {
 		overshoots++;
@@ -484,6 +637,27 @@ function checkOvershoot(prevItem) {
 
 // layout of menus
 function setMenuLayout() {
+	ss_menus = document.querySelectorAll('#ss_menu > div');
+	ss_num = ss_menus.length;
+	var ss_seg_ang = visualConditions[trials[currTrial].visualCondition - 1];
+	var page_center_x = 180, page_center_y = 180, page_radius = 130;
+	console.log(ss_menu.getBoundingClientRect().top+":"+ss_menu.getBoundingClientRect().height+":"+ss_menu.getBoundingClientRect().left+":"+ss_menu.getBoundingClientRect().width);
+	for (var ss_itr = 0; ss_itr < ss_menus.length; ss_itr++) {
+		let
+		ss_menu = ss_menus[ss_itr];
+		ss_menu.style.top = page_center_y - ss_menu.getBoundingClientRect().top
+				- ss_menu.getBoundingClientRect().height / 2 - page_radius
+				* Math.cos(ss_seg_ang * ss_itr) + 'px';
+		ss_menu.style.left = page_center_x
+				- ss_menu.getBoundingClientRect().left
+				- ss_menu.getBoundingClientRect().width / 2 + page_radius
+				* Math.sin(ss_seg_ang * ss_itr) + 'px';
+		var img = document.querySelectorAll("#menu-img");
+		img[ss_itr].src = imgPaths[conditionNum][ss_itr];
+	}
+}
+
+function setTutorialMenuLayout() {
 	ss_menus = document.querySelectorAll('#ss_menu > div');
 	ss_num = ss_menus.length;
 	var ss_seg_ang = visualConditions[trials[currTrial].visualCondition - 1];
@@ -500,7 +674,7 @@ function setMenuLayout() {
 				- ss_menu.getBoundingClientRect().width / 2 + page_radius
 				* Math.sin(ss_seg_ang * ss_itr) + 'px';
 		var img = document.querySelectorAll("#menu-img");
-		img[ss_itr].src = imgPaths[conditionNum][ss_itr];
+		img[ss_itr].src = imgPaths[1][ss_itr];
 	}
 }
 
@@ -527,6 +701,14 @@ function loadTarget() {
 	targetImg.src = imgPaths[conditionNum][trials[currTrial].target - 1];
 	targetImg.style.visibility = "visible";
 }
+
+function loadTutorialTarget() {
+	var targetImg = document.querySelector("#target-img");
+	targetImg.src = imgPaths[1][trainingTargets[currTraining] - 1];
+	targetImg.style.visibility = "visible";
+}
+
+
 
 function selectionCheck(clickX, clickY) {
 	var targetImg = document.querySelector("#target-img").src;
@@ -585,6 +767,68 @@ function selectionCheck(clickX, clickY) {
 		}
 	}
 }
+
+function tutSelectionCheck(clickX, clickY) {
+	var targetImg = document.querySelector("#target-img").src;
+	var selectedPath = imgPaths[1][rotationCount];
+	if (targetImg.includes(selectedPath)) {
+		successFeedback.play();
+		// Check if block is complete
+		var data = {
+			timestamp : Date.now() - currDate,
+			success: true,
+			num_overshoots: overshoots,
+			selection_id: rotationCount + 1,
+			angle : currAngle,
+			target_id : trainingTargets[currTraining],
+			curr_motor: trials[currTrial].motorCondition,
+			curr_visual: trials[currTrial].visualCondition,
+			curr_block: 0,
+			num_pos15: num_cw,
+			num_neg15: num_ccw,
+			curr_ef: trials[currTrial].eyesFreeCondition
+		};
+		logSummary(data);
+		iterateTutorial();
+		motorRotationCount = 0;
+		overshoots = 0;
+		clearTimeout(timeoutHandler);
+		if (currTraining == trainingTargets.length) {
+			isTraining = false;
+			resetMenu();
+		}
+		// Log Selection Data
+	} else {
+		failFeedback.play();
+		// Log penalty, go on to the next item
+		var data = {
+			timestamp : Date.now() - currDate,
+			success: false,
+			num_overshoots: overshoots,
+			selection_id: rotationCount + 1,
+			angle : currAngle,
+			target_id : trainingTargets[currTraining],
+			curr_motor: trials[currTrial].motorCondition,
+			curr_visual: trials[currTrial].visualCondition,
+			curr_block: 0,
+			num_pos15: num_cw,
+			num_neg15: num_ccw,
+			curr_ef: trials[currTrial].eyesFreeCondition
+		};
+		logSummary(data);
+		motorRotationCount = 0;
+		overshoots = 0;
+		firstRotationBool = false;
+		clearTimeout(timeoutHandler);
+		rotationCount = 0;
+		highLight(rotationCount, ss_menus);
+		if (trials[currTrial].eyesFreeCondition == true) {
+			$('#ss_menu').hide();
+			menuVisible = false;
+			firstRotationBool = false;
+		}
+	}
+}
 	
 function iterateTarget() {
 	currTrial++;
@@ -612,6 +856,31 @@ function iterateTarget() {
 	highLight(rotationCount, ss_menus);
 }
 
+function iterateTutorial() {
+	currTraining++;
+	if(trainingTargets[currTraining]){
+		if ((currTraining % 8) == 0) {
+			inTrial = false;
+		}
+		else {
+			$("#target-img").attr('src', imgPaths[1][trainingTargets[currTraining] - 1]);
+			if (trials[currTrial].eyesFreeCondition == true) {
+				$('#ss_menu').hide();
+				menuVisible = false;
+				firstRotationBool = false;
+			}
+		}
+	} else {
+		inTrial = false;
+		window.removeEventListener("rotarydetent", tutorialRotaryEventHandler);
+	}
+	removeHighlight();
+	rotationCount = 0;
+	num_cw = 0;
+	num_ccw = 0;
+	highLight(rotationCount, ss_menus);
+}
+
 window.onload = function() {
 	state = document.getElementById("trial-state");
 	failFeedback = document.getElementById("fail");
@@ -619,6 +888,6 @@ window.onload = function() {
 	document.querySelector("#target-img").style.visibility = "hidden";
 	setupStudy();
 	currCondition = document.getElementById("trial-condition");
-	state.innerHTML = "Start";
+	state.innerHTML = "Tutorial Start";
 	document.addEventListener("click", clickEvent);
 }
